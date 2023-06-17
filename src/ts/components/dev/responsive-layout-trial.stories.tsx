657c15bb6931e09716e785c1b8760e32b1533f91
Daniel Kauer <mail@danielkauer.at>
2023-06-17 16:52:14 +0200

    add stub and story for ResponsiveLayoutTrial

---
+++ src/ts/components/dev/responsive-layout-trial.stories.tsx
@@ -0,0 +1,8 @@
+import { ResponsiveLayoutTrial } from "./responsive-layout-trial"
+
+export default {
+  title: 'dkat4/Dev Components'
+}
+
+export const Story = () => <ResponsiveLayoutTrial/>
+Story.storyName = 'ResponsiveLayoutTrial'
---
+++ src/ts/components/dev/responsive-layout-trial.tsx
@@ -0,0 +1,5 @@
+export const ResponsiveLayoutTrial = (props:any) => {
+	return	<div>
+				Responsive Layout Trial
+			</div>
+}
