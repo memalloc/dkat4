401ecf1a143e3264f871310b4bb657750e45b6a2
Daniel Kauer <mail@danielkauer.at>
2023-06-23 14:02:43 +0200

    use single file for stories of dev components

--- src/ts/components/dev/dev-component.stories.tsx
+++ src/ts/components/dev/dev-component.stories.tsx
@@ -3,9 +3,13 @@
 // import styled from 'styled-components'
 
 import { LoremIpsum } from "./lorem-ipsum"
+import { ResponsiveLayoutTrial } from "./responsive-layout-trial"
 
 export default {
   title: 'dkat4/Dev Components'
 }
 
 export const LoremIpsumStory = () => <LoremIpsum/>
+
+export const ResponsiveLayout = () => <ResponsiveLayoutTrial/>
+ResponsiveLayout.storyName = 'ResponsiveLayoutTrial'
--- src/ts/components/dev/responsive-layout-trial.stories.tsx
+++
@@ -1,8 +0,0 @@
-import { ResponsiveLayoutTrial } from "./responsive-layout-trial"
-
-export default {
-  title: 'dkat4/Dev Components'
-}
-
-export const Story = () => <ResponsiveLayoutTrial/>
-Story.storyName = 'ResponsiveLayoutTrial'
