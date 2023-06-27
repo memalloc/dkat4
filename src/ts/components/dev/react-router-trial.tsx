8367bf569f841c14d3c93a29600c700f9417e1de
Daniel Kauer <mail@danielkauer.at>
2023-06-27 17:48:41 +0200

    implement motion transitions when changing route
    see https://stackoverflow.com/questions/70239122/react-router-dom-v6-with-framer-motion-v4/70239392#70239392

--- src/ts/components/dev/react-router-trial.tsx
+++ src/ts/components/dev/react-router-trial.tsx
@@ -1,22 +1,39 @@
-import { HashRouter, Link, Route, Routes } from 'react-router-dom'
+import { HashRouter, Link, Route, Routes, useLocation } from 'react-router-dom'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export const ReactRouterTrial = () => {
 
-	const baseContent = <div>
-									root level
-									<Link to={'/project'}>navigate to project level</Link>	
-								</div>
-	const projectContent = <div>
-									project level
-									<Link to={'..'} relative='path'>up/back</Link>	
-								</div>
 	return <div>	
 				<h1>react-router-dom test</h1>
 				<HashRouter>
-					<Routes>	
-						<Route path='/' element={baseContent}/>
-						<Route path='/project' element={projectContent}/>
-					</Routes>
+					<AnimatedRouter/>
 				</HashRouter>
 			</div>
+}
+
+const AnimatedRouter = () => {
+
+	const location = useLocation()
+
+	const BaseContent = <motion.div initial={{opacity:0}}
+											animate={{opacity:1}}
+											transition={{duration:0.5}}
+											exit={{opacity:0}}>
+									root level
+									<Link to={'/project'}>navigate to project level</Link>	
+								</motion.div>
+	const ProjectContent = <motion.div initial={{opacity:0}}
+											animate={{opacity:1}}
+											transition={{duration:0.5}}
+											exit={{opacity:0}}>
+									project level
+									<Link to={'..'} relative='path'>up/back</Link>	
+								</motion.div>
+
+	return <AnimatePresence mode='wait'>
+				<Routes location={location} key={location.pathname}>
+					<Route path='/' element={BaseContent}/>
+					<Route path='/project' element={ProjectContent}/>
+				</Routes>
+			</AnimatePresence>
 }
