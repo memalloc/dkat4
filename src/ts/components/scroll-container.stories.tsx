ff04cfd8f12d3d9f98b8fcf1282a41964fbfeb2c
Daniel Kauer <mail@danielkauer.at>
2023-07-11 11:48:37 +0200

    add ScrollContainer stub and story

---
+++ src/ts/components/scroll-container.stories.tsx
@@ -0,0 +1,7 @@
+import { ScrollContainer } from "./scroll-container"
+
+export default {
+  title: 'dkat4/UI'
+}
+
+export const ScrollContainerStory = () => <ScrollContainer/>
---
+++ src/ts/components/scroll-container.tsx
@@ -0,0 +1,12 @@
+import { styled } from 'styled-components'
+
+export const ScrollContainer = (props:any) => {
+
+	return	<Container>
+				ScrollContainer
+			</Container>
+}
+
+const Container = styled.div`
+	background: #777;
+`
