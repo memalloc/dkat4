a4d7fecb62fe6c518638950a95ef61a1ca44fb48
Daniel Kauer <mail@danielkauer.at>
2023-07-11 14:27:36 +0200

    add ResponsiveScrollContainer story

--- src/ts/components/scroll-container.stories.tsx
+++ src/ts/components/scroll-container.stories.tsx
@@ -2,6 +2,7 @@ import { styled } from 'styled-components'
 
 import { ScrollContainer } from "./scroll-container"
 import { LoremIpsum } from './dev/lorem-ipsum'
+import * as Design from '../design'
 
 export default {
 	title: 'dkat4/UI'
@@ -24,4 +25,31 @@ const ContainerOne = styled.div`
 	bottom: 10vh;
 
 	background: #777;
+`
+
+export const ResponsiveScrollContainer = () => {
+	return <ResponsiveContainer>
+					<ScrollContainer>
+						<LoremIpsum paragraphs={10}/>
+					</ScrollContainer>
+				</ResponsiveContainer>
+}
+
+
+const ResponsiveContainer = styled.div`
+	position: absolute;
+	top: 10vh;
+	left: 10vw;
+	width: 70vw;
+	bottom: 10vh;
+
+	background: #777;
+
+	@media (${Design.onMobile}) {
+		position: static;
+		top: unset;
+		left: unset;
+		bottom: unset;
+		width: unset;
+	}
 `
