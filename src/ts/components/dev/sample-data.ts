668388d1dc91ab6c2a9539a78dba201756122bc1
Daniel Kauer <mail@danielkauer.at>
2023-07-12 19:21:33 +0200

    add sample-data.ts to remove story created by export

---
+++ src/ts/components/dev/sample-data.ts
@@ -0,0 +1,11 @@
+import { ProjectData } from '../project-details'
+
+import TestVideo from '../../../assets/video/test.mp4'
+import { text } from './lorem-ipsum'
+
+export const sampleProject:ProjectData = {
+	title : `Verkehrsmuseum Remise`,
+	info : `Work: Design & Development  \nClient: Wiener Linien  \nAgency: [Zone Media](https://www.zonemedia.at/)`,
+	description : `Project Description, [dkat](https://www.danielkauer.at/) ${text}\n\n${text}`,
+	media : [TestVideo, 'https://placehold.co/800x800', 'https://placehold.co/1920x1080', 'https://placehold.co/90x160']
+}
--- src/ts/components/project-details.stories.tsx
+++ src/ts/components/project-details.stories.tsx
@@ -1,9 +1,7 @@
 import { styled } from 'styled-components'
 
-import { ProjectDetails, ProjectData } from './project-details'
-
-import TestVideo from '../../assets/video/test.mp4'
-import { text } from './dev/lorem-ipsum'
+import { ProjectDetails } from './project-details'
+import { sampleProject } from './dev/sample-data'
 
 export default {
 	title: 'dkat4/UI'
@@ -15,13 +13,6 @@ export const ProjectDetailsStory = () => {
 			</Container>
 }
 ProjectDetailsStory.storyName = 'Project Details'
-
-export const sampleProject:ProjectData = {
-	title : `Verkehrsmuseum Remise`,
-	info : `Work: Design & Development  \nClient: Wiener Linien  \nAgency: [Zone Media](https://www.zonemedia.at/)`,
-	description : `Project Description, [dkat](https://www.danielkauer.at/) ${text}\n\n${text}`,
-	media : [TestVideo, 'https://placehold.co/800x800', 'https://placehold.co/1920x1080', 'https://placehold.co/90x160']
-}
 
 const Container= styled.div`
 `
