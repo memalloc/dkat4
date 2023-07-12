148988688f3f1dccee760b2d38fba13d679b3f80
Daniel Kauer <mail@danielkauer.at>
2023-07-12 16:53:33 +0200

    implement MediaContent to output fitting element based on URL

---
+++ src/ts/components/media-content.tsx
@@ -0,0 +1,29 @@
+import { styled } from 'styled-components'
+
+import * as Design from '../design'
+
+interface Props {
+	url : string	
+}
+
+export const MediaContent = (props:Props) => {
+	if(props.url.includes('.mp4')){
+		return <Video src={props.url} autoPlay loop muted playsInline/>
+	} else {
+		return <Image src={props.url}/>
+	}
+}
+
+const Video = styled.video`
+	width: 100%;
+	max-height: 100vh;
+`
+
+const Image = styled.img`
+	max-width: 100%;
+	max-height: 100vh;
+
+	@media (${Design.onMobile}) {
+		width: 100%;
+	}
+`
