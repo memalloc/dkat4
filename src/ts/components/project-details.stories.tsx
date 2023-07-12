8fbad11ebde19cc5012d584c8d18cea2f62f62be
Daniel Kauer <mail@danielkauer.at>
2023-07-12 17:27:46 +0200

    increase text length of sample project

--- src/ts/components/project-details.stories.tsx
+++ src/ts/components/project-details.stories.tsx
@@ -19,7 +19,7 @@ ProjectDetailsStory.storyName = 'Project Details'
 const project:Project = {
 	title : `Verkehrsmuseum Remise`,
 	info : `Work: Design & Development  \nClient: Wiener Linien  \nAgency: [Zone Media](https://www.zonemedia.at/)`,
-	description : `Project Description, [dkat](https://www.danielkauer.at/) ${text}`,
+	description : `Project Description, [dkat](https://www.danielkauer.at/) ${text}\n\n${text}`,
 	media : [TestVideo, 'https://placehold.co/800x800', 'https://placehold.co/1920x1080', 'https://placehold.co/90x160']
 }
 
