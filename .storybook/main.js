8ba7aa66d61555970c35e197a701c1eff75f2519
Daniel Kauer <mail@danielkauer.at>
2023-07-12 16:25:39 +0200

    remove storybook styling addon from config since it is currently not installed

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,6 @@ const config = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-styling"
   ],
   framework: {
     name: "@storybook/react-vite",
