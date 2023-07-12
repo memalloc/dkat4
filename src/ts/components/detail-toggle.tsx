f466f509ef651d252952340e76b3b232640cc86c
Daniel Kauer <mail@danielkauer.at>
2023-07-12 17:30:02 +0200

    fix TSX SVG markup errors

--- src/ts/components/detail-toggle.tsx
+++ src/ts/components/detail-toggle.tsx
@@ -46,7 +46,7 @@ const Container = styled.div`
 
 const Icon = (props:{color:string}) => {
 	return <svg width="56px" height="56px" viewBox="0 0 56 56" version="1.1" xmlns="http://www.w3.org/2000/svg">
-				<g id="detail-toggle" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+				<g id="detail-toggle" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
 					<path d="M28,1 C42.9116882,1 55,13.0883118 55,28 C55,42.9116882 42.9116882,55 28,55 C13.0883118,55 1,42.9116882 1,28 C1,13.0883118 13.0883118,1 28,1 Z M27.7071068,9.10050506 L8.61522369,28.1923882 L17.8076118,37.3847763 L28,47.6066017 L27.9032237,34.9995051 L43,35 L43,22 L27.8042237,21.9995051 L27.7071068,9.10050506 Z" id="Combined-Shape" fill={props.color}></path>
 				</g>
 			</svg>
