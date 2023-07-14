ac718fd3effff38d6eec14a0534bff5c868c9be2
Daniel Kauer <mail@danielkauer.at>
2023-07-14 18:20:35 +0200

    implement hover for DetailToggle

--- src/ts/components/detail-toggle.tsx
+++ src/ts/components/detail-toggle.tsx
@@ -2,6 +2,7 @@ import { motion } from 'framer-motion'
 import { styled } from 'styled-components'
 
 import * as Design from '../design'
+import { useState } from 'react'
 
 interface Props {
 	details : boolean
@@ -9,6 +10,8 @@ interface Props {
 }
 
 export const DetailToggle = (props:Props) => {
+
+	const [hover, setHover] = useState(false)
 
 	const variants = {
 		initial : {
@@ -32,11 +35,14 @@ export const DetailToggle = (props:Props) => {
 		}
 	}
 
-	return	<Container>
+	return	<Container onMouseEnter={() => setHover(true)}
+						onMouseOver={() => setHover(true)}
+						onMouseOut={() => setHover(false)}
+						onMouseLeave={()=> setHover(false)}>
 				<motion.div onClick={props.onClick}
 							variants={variants}
 							animate={props.details ? 'details' : 'hidden'}>
-					<Icon color={Design.Colors.Orange}/>
+					<Icon color={Design.Colors.Orange} hover={hover}/>
 				</motion.div>
 			</Container>
 }
@@ -44,12 +50,15 @@ export const DetailToggle = (props:Props) => {
 const Container = styled.div`
 	width: 56px;
 	height: 56px;
+
+	cursor: pointer;
 `
 
-const Icon = (props:{color:string}) => {
+const Icon = (props:{color:string, hover:boolean}) => {
 	return <svg width="56px" height="56px" viewBox="0 0 56 56" version="1.1" xmlns="http://www.w3.org/2000/svg">
-				<g id="detail-toggle" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-					<path d="M28,1 C42.9116882,1 55,13.0883118 55,28 C55,42.9116882 42.9116882,55 28,55 C13.0883118,55 1,42.9116882 1,28 C1,13.0883118 13.0883118,1 28,1 Z M27.7071068,9.10050506 L8.61522369,28.1923882 L17.8076118,37.3847763 L28,47.6066017 L27.9032237,34.9995051 L43,35 L43,22 L27.8042237,21.9995051 L27.7071068,9.10050506 Z" id="Combined-Shape" fill={props.color}></path>
+				<g id="detail-toggle" stroke="none" strokeWidth="2" fill="none" fillRule="evenodd">
+        			<path d="M27.7071068,9.10050506 L27.8042237,21.9995051 L43,22 L43,35 L27.9032237,34.9995051 L28,47.6066017 L17.8076118,37.3847763 L8.61522369,28.1923882 L27.7071068,9.10050506 Z" id="Combined-Shape" fill={props.hover ? props.color : 'none'}></path>
+					<path d="M28,1 C42.9116882,1 55,13.0883118 55,28 C55,42.9116882 42.9116882,55 28,55 C13.0883118,55 1,42.9116882 1,28 C1,13.0883118 13.0883118,1 28,1 Z M27.7071068,9.10050506 L8.61522369,28.1923882 L17.8076118,37.3847763 L28,47.6066017 L27.9032237,34.9995051 L43,35 L43,22 L27.8042237,21.9995051 L27.7071068,9.10050506 Z" id="Combined-Shape" fill={!props.hover ? props.color : 'none'} stroke={props.hover ? props.color : 'none'}></path>
 				</g>
 			</svg>
 }
