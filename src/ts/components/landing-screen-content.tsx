9071fc4369538da2136ffda257f7746e39967e78
Daniel Kauer <mail@danielkauer.at>
2023-07-17 19:00:30 +0200

    add placeholder content for LandingScreenContent

--- src/ts/components/landing-screen-content.tsx
+++ src/ts/components/landing-screen-content.tsx
@@ -1,12 +1,31 @@
+import { useContext } from 'react'
 import styled from 'styled-components'
+import { ColorThemeContext } from '../app'
 
 interface Props {
 	onScrollToProjects : ()=>void	
 }
 
 export const LandingScreenContent = (props:Props) => {
+
+	const colorTheme = useContext(ColorThemeContext)
+
 	return	<>
-				initial screen content placeholder!
+				<MainText $color={colorTheme.primary}>
+					<Line>
+						daniel kauer
+					</Line>
+					<Line>
+						designs &amp; develops
+					</Line>
+					<Line>
+						pinboard twitter vimeo
+					</Line>
+					<Line>
+						say &gt;hello
+					</Line>
+				</MainText>
+
 				<Button onClick={()=>{
 					props.onScrollToProjects()
 				}}>
@@ -15,12 +34,38 @@ export const LandingScreenContent = (props:Props) => {
 			</>
 }
 
+const MainText = styled.div<{$color:string}>`
+	color: ${props => props.$color};
+
+	text-align: right;
+
+	position: absolute;
+	right: 3vw;
+	top: 0vh;
+	bottom: 0vh;
+
+	display: grid;
+    align-content: center;
+    font-family: ArvoRegular;
+
+    transition: 1s color;
+`
+
+const Line = styled.div`
+`
+
 // - - - - - - - - - - - - - - - - - - - - - - - - temp
 
 const Button = styled.div`
+	position: absolute;
+	bottom: 10vh;
+	left: 10vh;
+
+    font-family: ArvoBold;
+
 	color: #555;
 	background: #bbb;
-	padding: 5px;
+	padding: 15px;
 
 	display: inline;
 
