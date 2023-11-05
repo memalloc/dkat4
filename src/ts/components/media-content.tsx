1931cc51853a39dc52e40b31c559823516aea818
Daniel Kauer <mail@danielkauer.at>
2023-11-05 17:10:23 +0100

    update SVG video poster animation

--- src/ts/components/media-content.tsx
+++ src/ts/components/media-content.tsx
@@ -104,6 +104,11 @@ const Image = styled.img`
 `
 
 const svg = (theme:Design.ColorTheme) => {
+	const shapeColor =	theme.background
+	const bgColor = theme.secondaryBackground ?
+						 theme.secondaryBackground :
+						 theme.primary
+
 	return svgToMiniDataURI(`
 		<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 			 viewBox="0 0 1920 1080" enable-background="new 0 0 512 512" xml:space="preserve" width="10vw" height="10vw">
@@ -113,22 +118,20 @@ const svg = (theme:Design.ColorTheme) => {
 
 			<g opacity="0">
 				<animate attributeName="opacity" from="0" to="1" dur="1s" begin="1s" fill="freeze" />
-				<rect width="1917" height="1080" style="fill:${theme.background};opacity:0.6" />
-				<rect width="1917" height="1080" style="fill:transparent;stroke:${theme.primary};stroke-width:3;" />
 
 				<g id="logo">
-					<animate attributeName="opacity" values="0.2;0.7;0.2" dur="3s" repeatCount="indefinite"/>
+					<animate attributeName="opacity" values="0.8;0.9;0.8" dur="3s" repeatCount="indefinite"/>
 					<animateTransform	attributeName="transform" type="scale" 
-										values="0.7;1;0.7" begin="0s" dur="3s" repeatCount="indefinite"/>
+										values="0.3;0.5;0.3" begin="0s" dur="3s" repeatCount="indefinite"/>
 
 					<g transform="translate(512,192)" transform-origin="center">
 						<g transform="scale(0.7)" transform-origin="center">
-							<circle cx="256" cy="256" r="256"  fill="${theme.primary}"/>
+							<circle cx="256" cy="256" r="256"  fill="${bgColor}"/>
 						</g>
 						<g transform="scale(0.5) translate(-290 -130)" transform-origin="center">
-							<path fill="${theme.background}" d="M335.1,229.1v106.7h0c88.4,0,160-71.6,160-160H388.4C388.4,205.2,364.6,229.1,335.1,229.1z"/>
-							<path fill="${theme.background}" d="M388.4,495.7h106.7v0c0-88.4-71.6-160-160-160v106.7C364.6,442.4,388.4,466.2,388.4,495.7z"/>
-							<path fill="${theme.background}" d="M175.1,282.4L175.1,282.4c88.4,0,160-71.6,160-160V15.8H228.5v106.7c0,29.5-23.9,53.3-53.3,53.3
+							<path fill="${shapeColor}" d="M335.1,229.1v106.7h0c88.4,0,160-71.6,160-160H388.4C388.4,205.2,364.6,229.1,335.1,229.1z"/>
+							<path fill="${shapeColor}" d="M388.4,495.7h106.7v0c0-88.4-71.6-160-160-160v106.7C364.6,442.4,388.4,466.2,388.4,495.7z"/>
+							<path fill="${shapeColor}" d="M175.1,282.4L175.1,282.4c88.4,0,160-71.6,160-160V15.8H228.5v106.7c0,29.5-23.9,53.3-53.3,53.3
 								c-88.4,0-160,71.6-160,160c0,88.4,71.6,160,160,160s160-71.6,160-160H228.5c0,29.5-23.9,53.3-53.3,53.3c-29.4,0-53.3-23.9-53.3-53.3 C121.8,306.3,145.7,282.4,175.1,282.4z"/>
 						</g>
 					</g>
